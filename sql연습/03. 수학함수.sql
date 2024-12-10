75ecc502ee08329335ad44c7e94d723f2b270a3d
seojihyeon99 <seojihyeon99@naver.com>
2024-12-10 16:56:20 +0900

    241210 sql 연습 (select, 수학, 문자열, 날짜, 타입, 집계)

--- sql연습/03. 수학함수.sql
+++ sql연습/03. 수학함수.sql
@@ -1,6 +1,6 @@
 --
 -- 수학 함수
--- 
+--
 
 -- abs
 select abs(1), abs(-1) from dual;
@@ -15,13 +15,13 @@ select floor(3.14) from dual;
 select mod(10, 3), 10 % 3 from dual;
 
 -- round(x): x에 가장 근접한 정수
--- round(x, d): x값 중에 소수점 d자리에 가장 근접한 실수
+-- round(x, d): x값 중에 소수잠 d자리에 가장 근접한 실수
 select round(1.498), round(1.501), round(1.498, 1), round(1.498, 0) from dual;
 
 -- power(x, y), pow(x, y): x의 y승
 select power(2, 10), pow(2, 10) from dual;
 
--- sing(x): 양수 1, 음수 -1, 0 0
+-- sign(x) : 양수 1, 음수 -1, 0 0
 select sign(20), sign(-100), sign(0) from dual;
 
 -- greatest(x, y, ...), least(x, y, ...)
